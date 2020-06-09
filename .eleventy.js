92be0a3640180d607a2475887756a8182a954054
Budi Irawan <deerawan@gmail.com>
2020-06-09 21:05:56 +1000

    add rss plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 
 module.exports = function(config) {
@@ -16,6 +17,9 @@ module.exports = function(config) {
 
   // add support for syntax highlighting
   config.addPlugin(syntaxHighlight);
+
+  // add RSS plugin
+  config.addPlugin(pluginRss);
 
   // minify the html output
   config.addTransform("htmlmin", require("./src/utils/minify-html.js"));
